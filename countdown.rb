fdfb34b73ea7623aa1831e79a13270294c708e31
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-06 18:44:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,23 +1,10 @@
 #write your code here
 
 def countdown(number)
-  while number > 0
-  puts "#{number} SECOND(S)!"
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
   
   
 end
 
-def unsafe(speed)
-  if speed < 40 && speed > 60
-    return true
-  else
-    return false
-  end
-end
-
-def unsafe?(speed)
-	if speed < 40 && speed > 60
-	  return true
-  elsif
-  return false
-end
