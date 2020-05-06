50abc5f12ba3e9598eee2dd3a3e146a124267cbe
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-06 18:41:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,18 @@ def countdown(number)
   
   
 end
+
+def unsafe(speed)
+  if speed < 40 && speed > 60
+    return true
+  else
+    return false
+  end
+end
+
+def unsafe?(speed)
+	if speed < 40 && speed > 60
+	  return true
+  elsif
+  return false
+end
